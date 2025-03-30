03390262ee75883d97edeb7b058ddb7abd43c6fe
Jacob L <lecoqjacob@gmail.com>
2025-03-29 20:05:18 -0500

    add astrocommuinity plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,10 +1,31 @@
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.yaml" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+
+  { import = "astrocommunity.completion.blink-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.supermaven-nvim" },
+
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
+
+  { import = "astrocommunity.fuzzy-finder.fzf-lua" },
+
+  { import = "astrocommunity.recipes.astrolsp-auto-signature-help" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.recipes.picker-nvchad-theme" },
 }
